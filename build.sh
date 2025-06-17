e563c802d01f3573266b65a94422f5290c73bf04
LoreHan64 <dgdgdgdgppp@gmail.com>
2025-06-17 21:28:40 +0800

    update build.sh

--- build.sh
+++ build.sh
@@ -2,13 +2,13 @@ sudo apt-get update
 sudo apt-get install upx git curl unzip -y
 LATEST_VERSION=$(curl -s https://mirrors.tuna.tsinghua.edu.cn/termux/apt/termux-main/pool/main/a/alist/ | grep -oE 'title="([^"]+)"' | grep -oE '".+"$' | tr -d '"' | sed -n '2p' | head -n 1 | cut -d _ -f 2)
 echo $LATEST_VERSION
-LATEST_DOWNLOAD_URL=https://mirrors.tuna.tsinghua.edu.cn/termux/apt/termux-main/pool/main/a/alist/alist_${LATEST_VERSION}_aarch64.deb
+LATEST_DOWNLOAD_URL=https://github.com/AlliotTech/openalist/releases/download/beta/alist-linux-arm64.tar.gz
 echo $LATEST_DOWNLOAD_URL
 mkdir temp
 cd temp
-curl -s -k $LATEST_DOWNLOAD_URL -o alist.deb
-dpkg -X alist.deb alist_extract
-alist_bin_path=$(pwd)/alist_extract/data/data/com.termux/files/usr/bin/alist
+curl -s -k $LATEST_DOWNLOAD_URL -o alist.tar.gz
+tar -x alist.tar.gz alist_extract
+alist_bin_path=$(pwd)/alist_extract/alist
 upx -9 ${alist_bin_path} -o alist_${LATEST_VERSION}_aarch64_upx
 mv alist_${LATEST_VERSION}_aarch64_upx ../bin/alist
 cd ..
